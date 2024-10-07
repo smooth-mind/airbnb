7e27ceb04d528a3d19c01c8b168bea867d839cb3
Abdullah-Raffay <abdullahraffay1217@gmail.com>
2024-10-07 23:58:03 +0500

    "Added Search component to Navbar and installed react-icons dependency"

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo"
+import Search from "./Search";
 const Navbar = () => {
     return(
         <div className = "fixed w-full bg-white z-10 shadow-sm"> 
@@ -11,6 +12,7 @@ const Navbar = () => {
                         className="flex flex-row items-center justify-between gap-3 md:gap-0"
                     >
                         <Logo />
+                        <Search />
 
                     </div>
                 </Container>
